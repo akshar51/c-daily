86bbc1f7ebe1c57dc7e17efbe1fc3e8dbfe5323d
akshar66 <aksharparekh401@gmail.com>
2024-09-26 17:07:10 +0530

    done

--- C programming/Lecture12pattern/Q6.c
+++ C programming/Lecture12pattern/Q6.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 
 main(){
-	//         1
-	//       2 1
-	//     3 2 1
-	//   4 3 2 1
 	// 5 4 3 2 1
+	//   5 4 3 2
+	//     5 4 3
+	//       5 4
+	//         5
 	int i,j,k;
 	for(i=1;i<=5;i++){
-		for(k=5;k>i;k--){
+		for(k=i;k>1;k--){
 			printf("  ");
 		}
-		for(j=i;j>=1;j--){
+		for(j=5;j>=i;j--){
 			printf(" %d",j);
 		}
 		printf("\n");

978299ac12716c084f52653de8fa4a193e010588
akshar66 <aksharparekh401@gmail.com>
2024-09-30 12:01:05 +0530

    done

--- C programming/Circlet/Q7.c
+++ C programming/Circlet/Q7.c
@@ -6,5 +6,20 @@ main(){
 	// * * * * *
 	// *
 	// *
-	int i,j;
+	int i,j,k;
+	int n = 5;
+	for(i = 0; i < n; i++){
+    	printf("* ");
+    }
+  	printf("\n");
+  	printf("*");
+  	for(k = 1; k < n + 3; k++){
+   		printf(" ");
+  	}
+  	printf("*\n");
+  	for(i = 0; i < n; i++){
+    	printf("* ");
+    }
+  	printf("\n");
+  	printf("*\n*\n");
 }
--- C programming/Lecture12pattern/Q11.c
+++ C programming/Lecture12pattern/Q11.c
@@ -7,5 +7,13 @@ main(){
 	//    * * * * * * *
 	//  * * * * * * * * *
 	int i,j,space;
-	
+	for(i=1;i<=5;i++){
+		for(space=i;space<5;space++){
+			printf("  ");
+		}
+		for(j=1;j<=2*i-1;j++){
+			printf("* ");
+		}
+		printf("\n");
+	}
 }

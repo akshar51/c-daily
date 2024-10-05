717fdd4240311d1ab7b7aebc6f056d856ddac745
akshar66 <aksharparekh401@gmail.com>
2024-10-05 22:22:31 +0530

    done

--- C programming/Lecture15 String/Q3.c
+++ C programming/Lecture15 String/Q3.c
@@ -2,6 +2,16 @@
 #include<string.h>
 
 main(){
-	char str[11] = ("HELLO");
-	
+	int i=0;
+	char str[]="HelLo worLD";
+	while(str[i]!='\0'){
+		if(str[i]>=65 && str[i]<=90){
+			str[i]+=32;
+		}
+		else if(str[i]>=97 && str[i]<=122){
+			str[i]-=32;
+		}
+		i++;	
+	}
+	printf("%s",str);
 }

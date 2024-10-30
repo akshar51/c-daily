e97763eb89492e476cc00ba3000b6c4296a09245
akshar51 <aksharparekh401@gmail.com>
2024-10-30 17:09:35 +0530

    done

---
+++ Practise/p31.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main(){
+	// * * * * *
+	// * * * * *
+	// * * * * *
+	// * * * * *
+	// * * * * *
+	
+	int i=1,j;
+	while(i<=5){
+		j=1;
+		while(j<=5){
+		printf(" *");
+		j++;
+	}
+	printf("\n");
+	i++;
+	}
+}
